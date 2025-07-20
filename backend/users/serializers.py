5fadde9b5928daf0ddc8111c5ad7eb6314326054
vik-4994 <igor.gavrylenko05@gmail.com>
2025-07-20 21:09:27 +0200

    Added serializers for new tables in DB

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,17 +1,31 @@
 from django.contrib.auth.models import User
+from .models import TelegramUser, TrainingChannel
 from rest_framework import serializers
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ["username", "email", "password"]
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email', ''),
-            password=validated_data['password']
+            username=validated_data["username"],
+            email=validated_data.get("email", ""),
+            password=validated_data["password"],
         )
         return user
+
+
+class TelegramUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TelegramUser
+        fields = "__all__"
+
+
+class TrainingChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrainingChannel
+        fields = "__all__"
